2e75574ce160c70fb70e12fa10f509ed3320fcb7
Fernando Garate <fernando@devsar.com>
2017-10-06 15:46:45 -0300

    Add server app, requirements and data

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# frontend-boilerplate
+# Devsar frontend-boilerplate
+
+## Instalar dependencias
+`pip install -r requirements.txt`
+
+## Iniciar el servidor de desarrollo
+`python app.py`
+
+## Acceso y endpoints
+
+### Index page
+http://localhost:8080/
+
+### Listado de albums
+http://localhost:8080/api/v1/albums/
+
+### Detalle de un álbum
+http://localhost:8080/api/v1/albums/656801179/
+
+## Info
+Agrega tus archivos estáticos en la carpeta assets y comienza a desarrollar tu aplicación en index.html
+Para más información acerca de cómo instalar Python y configurar tu ambiente de trabajo: https://cloud.google.com/python/setup
---
+++ app.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+from bottle import Bottle, abort, run, view, static_file
+
+
+app = Bottle()
+
+
+@app.get("/")
+@view("index.html")
+def index():
+    return {}
+
+
+@app.get("/assets/<fname:path>")
+def assets(fname):
+    return static_file(fname, root="assets")
+
+
+@app.get("/api/v1/albums/")
+def albums():
+    fname = "data/albums/index.json"
+    if not os.path.exists(fname):
+        abort(404)
+    with open(fname, "rb") as fd:
+        return json.load(fd)
+
+
+@app.get("/api/v1/albums/<album_id:int>/")
+def album(album_id):
+    fname = "data/albums/{}.json".format(album_id)
+    if not os.path.exists(fname):
+        abort(404)
+    with open(fname, "rb") as fd:
+        return json.load(fd)
+
+
+if __name__ == "__main__":
+    run(app, host="localhost", port=8080)
